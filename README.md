0f3f2ae5723ab7c5a4319f44b6355f61727993b0
sbuckley <sbuckley65@hotmail.com>
2016-04-12 15:04:29 -0700

    Final files.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # About Me
 Assignment for Code 201 to create a personal profile page with a guessing game.
 
-I like popsicles.
+##What I did
+_Added index.htm and app.js
+_Tested site and it works.
--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 
+var college = prompt('Did I go to Iowa State University?').toUpperCase();
+if (college === 'Y') {
+  alert('Go Cyclones!!');
+} else {
+  alert('No, I\'m a Cyclone, darn it!');
+}
+
+var phones = prompt('Is one of my goals to work with mobile devices?').toUpperCase();
+if (phones === 'Y') {
+  alert('Thats right They are the future.');
+} else {
+  alert('Actually I really would like to work with them.');
+}
+
+var lab = prompt('Did I ever work as a lab tech?').toUpperCase();
+if (lab === 'Y') {
+  alert('Yes. You get extra knowledge. I worked with cockroaches!');
+} else {
+  alert('Sorry, but I acutally did work as a lab tech.');
+}
+
+var insurance = prompt('Did I ever work at an insurance company?').toUpperCase();
+if (insurance === 'Y') {
+  alert('Correct.');
+} else {
+  alert('Sorry, that\'s wrong.');
+}
