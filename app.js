e7976ed45e3bc7e1aeaf4c3b21c827a1fb39f501
sbuckley <sbuckley65@hotmail.com>
2016-04-13 14:42:46 -0700

    Added intro question for user name and updated old questions with Yes support and added user name.

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
+var userName = prompt('Let\'s play a game. Please enter your name and click OK.');
+console.log('User\'s name is ' + userName);
 
-var college = prompt('Did I go to Iowa State University?').toUpperCase();
-if (college === 'Y') {
+var college = prompt(userName + ', did I go to Iowa State University? (Yes/No)').toUpperCase();
+if (college === 'YES' || college === 'Y') {
   alert('Go Cyclones!!');
+  console.log('User said Yes.');
 } else {
   alert('No, I\'m a Cyclone, darn it!');
+  console.log('User said No.');
 }
 
-var phones = prompt('Is one of my goals to work with mobile devices?').toUpperCase();
-if (phones === 'Y') {
+var phones = prompt(userName + ', is one of my goals to work with mobile devices? (Yes/No)').toUpperCase();
+if (phones === 'YES' || phones === 'Y') {
   alert('Thats right They are the future.');
+  console.log('User said Yes.');
 } else {
   alert('Actually I really would like to work with them.');
+  console.log('User said No.');
 }
 
-var lab = prompt('Did I ever work as a lab tech?').toUpperCase();
-if (lab === 'Y') {
+var lab = prompt(userName + ', did I ever work as a lab tech? (Yes/No)').toUpperCase();
+if (lab === 'YES' || lab === 'Y') {
   alert('Yes. You get extra knowledge. I worked with cockroaches!');
+  console.log('User said Yes.');
 } else {
   alert('Sorry, but I acutally did work as a lab tech.');
+  console.log('User said No.');
 }
 
-var insurance = prompt('Did I ever work at an insurance company?').toUpperCase();
-if (insurance === 'Y') {
+var insurance = prompt(userName + ', did I ever work at an insurance company? (Yes/No)').toUpperCase();
+if (insurance === 'YES' || insurance === 'Y') {
   alert('Correct.');
+  console.log('User said Yes.');
 } else {
   alert('Sorry, that\'s wrong.');
+  console.log('User said No.');
 }
